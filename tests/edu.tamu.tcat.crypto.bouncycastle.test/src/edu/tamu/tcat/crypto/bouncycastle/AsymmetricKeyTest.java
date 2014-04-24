b5a803d934e0fac451cf55e905ea1cf7032453e8
Graham Booker <gbooker@tamu.edu>
2014-04-24 10:38:18 -0500

    Added test of known good signature.

--- tests/edu.tamu.tcat.crypto.bouncycastle.test/src/edu/tamu/tcat/crypto/bouncycastle/AsymmetricKeyTest.java
+++ tests/edu.tamu.tcat.crypto.bouncycastle.test/src/edu/tamu/tcat/crypto/bouncycastle/AsymmetricKeyTest.java
@@ -3,6 +3,7 @@ package edu.tamu.tcat.crypto.bouncycastle;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.nio.charset.Charset;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
@@ -12,10 +13,10 @@ import edu.tamu.tcat.crypto.CryptoProvider;
 import edu.tamu.tcat.crypto.DigestType;
 import edu.tamu.tcat.crypto.SignatureSigner;
 import edu.tamu.tcat.crypto.SignatureVerifier;
-import edu.tamu.tcat.crypto.bouncycastle.BouncyCastleCryptoProvider;
 
 public class AsymmetricKeyTest
 {
+   protected static final byte[] signedMessage = "Here is my message".getBytes(Charset.forName("UTF8"));
    protected CryptoProvider provider;
    
    @Before
@@ -24,16 +25,26 @@ public class AsymmetricKeyTest
       provider = new BouncyCastleCryptoProvider();
    }
    
+   public void testVerify(PublicKey publicKey, byte[] signature) throws Exception
+   {
+      SignatureVerifier verifier = provider.getSignatureBuilder().buildVerifier(publicKey, DigestType.SHA512, signature);
+      verifier.processData(signedMessage);
+      assertTrue(verifier.verify());
+      
+      verifier = provider.getSignatureBuilder().buildVerifier(publicKey, DigestType.SHA512, signature);
+      verifier.processData("This is my mad messages".getBytes());
+      assertFalse(verifier.verify());
+   }
+   
    public void testSign(PrivateKey privateKey, PublicKey publicKey) throws Exception
    {
       SignatureSigner signer = provider.getSignatureBuilder().buildSigner(privateKey, DigestType.SHA512);
       
-      byte[] message = "This is my message".getBytes();
-      signer.processData(message);
+      signer.processData(signedMessage);
       byte[] signature = signer.processFinal();
       
       SignatureVerifier verifier = provider.getSignatureBuilder().buildVerifier(publicKey, DigestType.SHA512, signature);
-      verifier.processData(message);
+      verifier.processData(signedMessage);
       assertTrue(verifier.verify());
       
       verifier = provider.getSignatureBuilder().buildVerifier(publicKey, DigestType.SHA512, signature);
